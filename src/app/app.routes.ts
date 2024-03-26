8314bee1ae9262a623ed11958c9fb8b9ab256659
Norbory <mandros.angelof@pucp.edu.pe>
2024-03-26 00:05:24 -0500

    Form Fully Functional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CartaComponent } from './components/carta/carta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'product/:id', component: CartaComponent },
+];
--- src/app/components/saludo/saludo.component.ts
+++ src/app/components/saludo/saludo.component.ts
@@ -1,35 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CartaComponent } from "../carta/carta.component";
 import { TableComponent } from '../table/table.component';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import * as data from '../../data/products.json';
 import { NgFor } from '@angular/common';
 import { Product } from '../../types/product';
 
 @Component({
-    selector: 'app-saludo',
-    standalone: true,
-    templateUrl: './saludo.component.html',
-    styleUrl: './saludo.component.css',
-    imports: [CartaComponent, TableComponent, NgFor, ReactiveFormsModule]
+  selector: 'app-saludo',
+  standalone: true,
+  templateUrl: './saludo.component.html',
+  styleUrl: './saludo.component.css',
+  imports: [CartaComponent, TableComponent, NgFor, ReactiveFormsModule],
 })
-export class SaludoComponent implements OnInit{
-    products = (data as any).default;
+export class SaludoComponent {
+  products = (data as any).default;
 
-    form: FormGroup = new FormGroup({
-        product: new FormControl(''),
-        stock: new FormControl(''),
-        price: new FormControl(''),
-        img: new FormControl(''),
-        url: new FormControl(''),
-        location: new FormControl('')
-      });
-    
-      ngOnInit() {
-        
-      }
-      
-      agregarProducto() {
-        console.log(this.form);
-      }
+  form = new FormGroup({
+    products: new FormControl(''),
+    location: new FormControl(''),
+    stock: new FormControl(0),
+    price: new FormControl(''),
+    img: new FormControl(''),
+    url: new FormControl(''),
+  });
+
+  agregarCarta() {
+    const product: Product = {
+      product: this.form.value.products ?? 'Producto',
+      location: this.form.value.location ?? 'Ubicación',
+      stock: this.form.value.stock ?? 0,
+      price: this.form.value.price ?? 'Precio',
+      img: this.form.value.img ?? 'Imagen',
+      url: this.form.value.url ?? 'URL',
+    };
+    this.products.push(product);
+    this.form.reset();
+  }
+
 } 
