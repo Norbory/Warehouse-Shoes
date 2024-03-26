316f3b181255385f8e89ed56aa51fcea4f80647c
Norbory <mandros.angelof@pucp.edu.pe>
2024-03-25 19:22:23 -0500

    Form incomplete

--- src/app/components/saludo/saludo.component.ts
+++ src/app/components/saludo/saludo.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartaComponent } from "../carta/carta.component";
 import { TableComponent } from '../table/table.component';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import * as data from '../../data/products.json';
 import { NgFor } from '@angular/common';
+import { Product } from '../../types/product';
 
 @Component({
     selector: 'app-saludo',
     standalone: true,
     templateUrl: './saludo.component.html',
     styleUrl: './saludo.component.css',
-    imports: [CartaComponent, TableComponent, NgFor]
+    imports: [CartaComponent, TableComponent, NgFor, ReactiveFormsModule]
 })
-export class SaludoComponent {
+export class SaludoComponent implements OnInit{
     products = (data as any).default;
-}
+
+    form: FormGroup = new FormGroup({
+        product: new FormControl(''),
+        stock: new FormControl(''),
+        price: new FormControl(''),
+        img: new FormControl(''),
+        url: new FormControl(''),
+        location: new FormControl('')
+      });
+    
+      ngOnInit() {
+        
+      }
+      
+      agregarProducto() {
+        console.log(this.form);
+      }
+} 
