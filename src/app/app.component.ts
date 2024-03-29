8ae08a9402991ffeb712359157d6417143ac7213
Norbory <mandros.angelof@pucp.edu.pe>
2024-03-29 09:12:40 -0500

    Added Navbar and fix router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SaludoComponent } from "./components/saludo/saludo.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, SaludoComponent]
+    imports: [RouterOutlet, SaludoComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'first_project';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductoComponent } from './components/producto/producto.component';
+import { SaludoComponent } from './components/saludo/saludo.component';
 
 export const routes: Routes = [
+    { path: '', component: SaludoComponent},
     { path: 'producto/:id', title:"Producto deportivo" ,component: ProductoComponent },
-];
+];  
