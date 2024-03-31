9806b302b8a3b4a6388d4649e19941d84c033355
Norbory <mandros.angelof@pucp.edu.pe>
2024-03-31 10:41:11 -0500

    Redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SaludoComponent } from "./components/saludo/saludo.component";
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, SaludoComponent, NavbarComponent]
+    imports: [RouterOutlet, SaludoComponent, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'first_project';
