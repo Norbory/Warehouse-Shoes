c38f2685d4bf1c9c5e9ccf4a3e8b3dc9e3cf0b8c
Norbory <mandros.angelof@pucp.edu.pe>
2024-04-04 14:14:19 -0500

    Comment every database CRUD method until deploy of API

--- src/app/components/saludo/saludo.component.ts
+++ src/app/components/saludo/saludo.component.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
 import * as data from '../../data/products.json';
 import { NgFor, NgIf } from '@angular/common';
 import { Product } from '../../types/product';
-import { ZapatillaService } from '../../services/zapatilla.service';
+// import { ZapatillaService } from '../../services/zapatilla.service';
 
 @Component({
   selector: 'app-saludo',
@@ -15,18 +15,22 @@ import { ZapatillaService } from '../../services/zapatilla.service';
   styleUrl: './saludo.component.css',
   imports: [CartaComponent, TableComponent, NgFor, NgIf, ReactiveFormsModule, RouterModule],
 })
-export class SaludoComponent implements OnInit{
-  constructor(private _zapatillaService: ZapatillaService) { }
+export class SaludoComponent {
+  // Configuracion para agregar Database
+  // implements OnInit
+  // private _zapatillaService: ZapatillaService
+  constructor() { }
   
-  ngOnInit() {
-    this.obtenerZapatillas();
-  }
+  // ngOnInit() {
+    // Llamada a la funcion para obtener zapatillas
+    // this.obtenerZapatillas();
+  // }
 
   listZapatillas: Product[] = [];
   submitted! : boolean;
   eliminated! : boolean;
   edited! : boolean;
-  products = (data as any).default;
+  productos = (data as any).default;
 
   form = new FormGroup({
     id: new FormControl(0),
@@ -38,16 +42,119 @@ export class SaludoComponent implements OnInit{
     url: new FormControl('', Validators.required),
   });
 
-  obtenerZapatillas() {
-    this._zapatillaService.getAllZapatillas().subscribe(
-      response => {
-        this.listZapatillas = response;
-      },
-      error => {
-        error.error.message = 'Error al obtener zapatillas';
-        console.log(error.error.message);
-      }
-    );
+  // Funcion para obtener cartas con integracion de API .NET 8  y base de datos SQL
+  // obtenerZapatillas() {
+  //   this._zapatillaService.getAllZapatillas().subscribe(
+  //     response => {
+  //       this.listZapatillas = response;
+  //     },
+  //     error => {
+  //       error.error.message = 'Error al obtener zapatillas';
+  //       console.log(error.error.message);
+  //     }
+  //   );
+  // }
+
+  // Funcion para agregar carta con integracion de API .NET 8  y base de datos SQL
+  // agregarCarta() {
+  //   const product: Product = {
+  //     id: this.form.value.id ?? 0,
+  //     product: this.form.value.products ?? 'Producto',
+  //     location: this.form.value.location ?? 'Ubicación',
+  //     stock: this.form.value.stock ?? 0,
+  //     price: this.form.value.price ?? 'Precio',
+  //     img: this.form.value.img ?? 'Imagen',
+  //     url: this.form.value.url ?? 'URL',
+  //   };
+  //   this._zapatillaService.createZapatilla(product).subscribe(
+  //     response => {
+  //       setTimeout(() => {
+  //         this.submitted = false;
+  //       }, 3000);
+  //       this.submitted = true;
+  //       this.form.reset();
+  //       this.obtenerZapatillas();
+  //     },
+  //     error => {
+  //       error.error.message = 'Error al crear zapatilla';
+  //       console.log(error.error.message);
+  //     }
+  //   );
+  // }
+
+  // Funcion para obtener zapatilla con integracion de API .NET 8  y base de datos SQL
+  // obtenerZapatilla(index: number) {
+  //   this._zapatillaService.getZapatillaById(index).subscribe(
+  //     response => {
+  //       this.form.setValue({
+  //         id: response.id,
+  //         products: response.product,
+  //         location: response.location,
+  //         stock: response.stock,
+  //         price: response.price,
+  //         img: response.img,
+  //         url: response.url,
+  //       });
+  //     },
+  //     error => {
+  //       error.error.message = 'Error al obtener zapatilla';
+  //       console.log(error.error.message);
+  //     }
+  //   );
+  // }
+
+  // Funcion para editar zapatilla con integracion de API .NET 8  y base de datos SQL
+  // editarCarta(index: number) {
+  //   this.obtenerZapatilla(index);
+  //   const product: Product = {
+  //     id: index,
+  //     product: this.form.value.products ?? 'Producto',
+  //     location: this.form.value.location ?? 'Ubicación',
+  //     stock: this.form.value.stock ?? 0,
+  //     price: this.form.value.price ?? 'Precio',
+  //     img: this.form.value.img ?? 'Imagen',
+  //     url: this.form.value.url ?? 'URL',
+  //   };
+  //   this._zapatillaService.updateZapatilla(product).subscribe(
+  //     response => {
+  //       setTimeout(() => {
+  //         this.edited = false;
+  //       }, 3000);
+  //       this.edited = true;
+  //       this.form.reset();
+  //       this.obtenerZapatillas();
+  //     },
+  //     error => {
+  //       error.error.message = 'Error al editar zapatilla';
+  //       console.log(error.error.message);
+  //     }
+  //   );
+  // }
+  
+  // Funcion para eliminar carta con integracion de API .NET 8  y base de datos SQL
+  // eliminarCarta(index: number) {
+  //   this._zapatillaService.deleteZapatilla(index).subscribe(
+  //     response => {
+  //       setTimeout(() => {
+  //         this.eliminated = false;
+  //       }, 3000);
+  //       this.eliminated = true;
+  //       this.obtenerZapatillas();
+  //     },
+  //     error => {
+  //       error.error.message = 'Error al eliminar zapatilla';
+  //       console.log(error.error.message);
+  //     }
+  //   );
+  // } 
+
+  enviarFormulario() {
+    const index = this.form.value.id ?? 0;
+    if (index === 0) {
+      this.agregarCarta();
+    } else {
+      this.editarCarta(index);
+    }
   }
 
   agregarCarta() {
@@ -60,43 +167,37 @@ export class SaludoComponent implements OnInit{
       img: this.form.value.img ?? 'Imagen',
       url: this.form.value.url ?? 'URL',
     };
-    this._zapatillaService.createZapatilla(product).subscribe(
-      response => {
-        setTimeout(() => {
-          this.submitted = false;
-        }, 3000);
-        this.submitted = true;
-        this.form.reset();
-        this.obtenerZapatillas();
-      },
-      error => {
-        error.error.message = 'Error al crear zapatilla';
-        console.log(error.error.message);
-      }
-    );
+    this.productos.push(product);
+    setTimeout(() => {
+      this.submitted = false;
+    }, 3000);
+    this.submitted = true;
+    this.form.reset();
+  }
+
+  eliminarCarta(index: number) {
+    this.productos.splice(index, 1);
+    setTimeout(() => {
+      this.eliminated = false;
+    }, 3000);
+    this.eliminated = true;
   }
 
   obtenerZapatilla(index: number) {
-    this._zapatillaService.getZapatillaById(index).subscribe(
-      response => {
-        this.form.setValue({
-          id: response.id,
-          products: response.product,
-          location: response.location,
-          stock: response.stock,
-          price: response.price,
-          img: response.img,
-          url: response.url,
-        });
-      },
-      error => {
-        error.error.message = 'Error al obtener zapatilla';
-        console.log(error.error.message);
-      }
-    );
+    const product = this.productos[index];
+    this.form.setValue({
+      id: product.id,
+      products: product.product,
+      location: product.location,
+      stock: product.stock,
+      price: product.price,
+      img: product.img,
+      url: product.url,
+    });
   }
 
   editarCarta(index: number) {
+    console.log(index);
     this.obtenerZapatilla(index);
     const product: Product = {
       id: index,
@@ -107,44 +208,11 @@ export class SaludoComponent implements OnInit{
       img: this.form.value.img ?? 'Imagen',
       url: this.form.value.url ?? 'URL',
     };
-    this._zapatillaService.updateZapatilla(product).subscribe(
-      response => {
-        setTimeout(() => {
-          this.edited = false;
-        }, 3000);
-        this.edited = true;
-        this.form.reset();
-        this.obtenerZapatillas();
-      },
-      error => {
-        error.error.message = 'Error al editar zapatilla';
-        console.log(error.error.message);
-      }
-    );
-  }
-
-  eliminarCarta(index: number) {
-    this._zapatillaService.deleteZapatilla(index).subscribe(
-      response => {
-        setTimeout(() => {
-          this.eliminated = false;
-        }, 3000);
-        this.eliminated = true;
-        this.obtenerZapatillas();
-      },
-      error => {
-        error.error.message = 'Error al eliminar zapatilla';
-        console.log(error.error.message);
-      }
-    );
-  } 
-
-  enviarFormulario() {
-    const index = this.form.value.id ?? 0;
-    if (index === 0) {
-      this.agregarCarta();
-    } else {
-      this.editarCarta(index);
-    }
+    this.productos[index] = product;
+    setTimeout(() => {
+      this.edited = false;
+    }, 3000);
+    this.edited = true;
+    this.form.reset();
   }
 }
